debfee4a0e2dbd31114f6155eab82a21e59a1dc0
Sourabh <saurav7767@gmail.com>
2023-12-31 15:17:46 +0530

    updates

--- routes/web.php
+++ routes/web.php
@@ -15,38 +15,38 @@ use App\Http\Controllers\Api\EmployeeController;
 */
 Route::get('/export', [EmployeeController::class, 'exportTimings'])->name('exportTimings');
 Route::get('/multi-export', [EmployeeController::class, 'exportTimingsMultiSheets'])->name('exportTimingsMultiSheets');
-Route::middleware('prevent-back-history')->group(function (){
+// Route::middleware('prevent-back-history')->group(function (){
 
-    Route::get('/clear-cache', function () {
-        Artisan::call('cache:clear');
-        Artisan::call('route:clear');
-        Artisan::call('config:clear');
-        Artisan::call('view:clear');
+//     Route::get('/clear-cache', function () {
+//         Artisan::call('cache:clear');
+//         Artisan::call('route:clear');
+//         Artisan::call('config:clear');
+//         Artisan::call('view:clear');
         
-        return Redirect::back()->with('success', 'All cache cleared successfully.');
-    });
+//         return Redirect::back()->with('success', 'All cache cleared successfully.');
+//     });
 
-    Auth::routes();
-    Route::middleware('auth')->group(function(){
+//     Auth::routes();
+//     Route::middleware('auth')->group(function(){
 
-        Route::get('/', 'HomeController@index')->name('user.home');
-        Route::resource('users', 'UserController');
-        Route::resource('role', 'RoleController');
-        Route::post('createRole','RoleController@createRole')->name('createRole');
-        Route::post('updateRole','RoleController@updateRole')->name('updateRole');
-        Route::get('/user/changeStatus/{id}','UserController@changeStatus')->name('user.changeStatus');
-        Route::get('user/profile','UserController@profile')->name('user.profile');
-        Route::get('user/update-profile','UserController@showUpdateProfileForm')->name('user.updateProfile');
-        Route::post('user/update-profile','UserController@updateProfile')->name('user.updateProfile.submit');
-        Route::get('user/change-password','UserController@changePasswordView')->name('user.changePassword');
-        Route::post('user/change-password','UserController@changePassword')->name('user.changePassword.submit');
+//         Route::get('/', 'HomeController@index')->name('user.home');
+//         Route::resource('users', 'UserController');
+//         Route::resource('role', 'RoleController');
+//         Route::post('createRole','RoleController@createRole')->name('createRole');
+//         Route::post('updateRole','RoleController@updateRole')->name('updateRole');
+//         Route::get('/user/changeStatus/{id}','UserController@changeStatus')->name('user.changeStatus');
+//         Route::get('user/profile','UserController@profile')->name('user.profile');
+//         Route::get('user/update-profile','UserController@showUpdateProfileForm')->name('user.updateProfile');
+//         Route::post('user/update-profile','UserController@updateProfile')->name('user.updateProfile.submit');
+//         Route::get('user/change-password','UserController@changePasswordView')->name('user.changePassword');
+//         Route::post('user/change-password','UserController@changePassword')->name('user.changePassword.submit');
 
-        Route::resource('page', 'PageController');
-        Route::post('upload', 'PageController@upload')->name('upload');
+//         Route::resource('page', 'PageController');
+//         Route::post('upload', 'PageController@upload')->name('upload');
 
         
 
      
 
-    });
-});
+//     });
+// });
